b70267ad9ce5bd549e9c9208ea50ec90b8985522
Joy Jiang <80540526+jiangj07@users.noreply.github.com>
2024-12-29 22:59:39 -0500

    Update train-loop-gpu-huge.py

--- train-loop-gpu-huge.py
+++ train-loop-gpu-huge.py
@@ -7,6 +7,20 @@ import torch
 import torch.nn as nn
 import copy
 import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+from torch.distributed import init_process_group
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+import sys
+
+sys.path.append('/sc/arion/projects/ECHO_ML/gulamf01/multimodal_LLM') # mmllm
+sys.path.append('/sc/arion/projects/ECHO_ML/gulamf01/waveform-dataloaders') # wfdm
+sys.path.append('/sc/arion/projects/ECHO_ML/gulamf01/jepa') # app AND src
+#os.environ['CUDA_LAUNCH_BLOCKING']="1"#
+# os.environ['TORCH_USE_CUDA_DSA'] = "1"
+
 from mmllm.dataloader.video import video_sampler, video_dataloader, video_transforms, video_collator
 from wfdl.angiography.dataloader import VideoDataset
 from mmllm.dataloader.utils import repeat_interleave_batch
@@ -16,21 +30,15 @@ from mmllm.models.utils import WarmupCosineSchedule, CosineWDSchedule, pretrain_
 from app.vjepa.utils import init_video_model
 from src.models.utils.multimask import MultiMaskWrapper, PredictorMultiMaskWrapper
 from src.masks.utils import apply_masks
-from torch.utils.tensorboard import SummaryWriter
-from torch.distributed import init_process_group
-import torch.multiprocessing as mp
-from torch.utils.data.distributed import DistributedSampler
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
 
 
 
 
 
-def main(config_kwargs):
+
+def main(rank, config_kwargs):
     
-    
-    rank = int(os.environ["LOCAL_RANK"])
+    # rank = int(os.environ["LOCAL_RANK"])
     config_path = config_kwargs.config_path
     # My dictionary. 
     print("Loading dictionary.")
@@ -59,6 +67,18 @@ def main(config_kwargs):
     shared_transform=config['shared_transform']
     snapshot_path=config['snapshot_path']
 
+    ranks = config['gpus']
+    world_size=config['world_size']
+    shuffle=config['shuffle']
+    ddp_setup_original(rank=rank, world_size=world_size)
+
+    print("Distributed initizlied", torch.distributed.is_initialized())
+    if torch.distributed.is_initialized():
+        gpu_id = torch.distributed.get_rank()
+        torch.cuda.set_device(gpu_id)
+        print("Current gpu", gpu_id)
+
+
     # if not torch.cuda.is_available():
     #     device = torch.device('cpu')
     # else:
@@ -66,9 +86,9 @@ def main(config_kwargs):
     #     torch.cuda.set_device(device)
 
     # Samplers
-    world_size=config['world_size']
+    
     # rank=config['rank']
-    shuffle=config['shuffle']
+    
 
     
 
@@ -116,7 +136,6 @@ def main(config_kwargs):
     )
 
     print("loading sampler.")
-
     unsupervised_sampler = video_sampler(
         dataset, world_size, rank, shuffle
     )
@@ -188,12 +207,7 @@ def main(config_kwargs):
     epsilon=config['epsilon']
     mixed_precision = config['mixed_precision']
 
-    print("Initializing optimizer.")
-    optimizer = pretrain_optimizer(
-        encoder=encoder, predictor=predictor, betas=betas, epsilon=epsilon, zero_init_bias_wd=zero_init_bias_wd
-    )
-    scaler = torch.cuda.amp.GradScaler() if mixed_precision else None
-
+    
 
     # Optimizers. 
     warmup=config['warmup']
@@ -212,6 +226,120 @@ def main(config_kwargs):
     clip_grad = None
 
     
+    
+    # Checkpointing
+    checkpoint_freq = config['checkpoint_frequency']
+    checkpoint_folder = config['checkpoint_folder']
+    load_model = config['load_model']
+    print("Loading from checkpoint.")
+    if load_model:
+        print("Initializing optimizer.")
+        optimizer = pretrain_optimizer(
+            encoder=encoder, predictor=predictor, betas=betas, epsilon=epsilon, zero_init_bias_wd=zero_init_bias_wd
+        )
+        scheduler = WarmupCosineSchedule(
+                optimizer,
+                warmup_steps=int(warmup * iterations_per_epoch),
+                start_lr=start_lr,
+                ref_lr=ref_lr,
+                final_lr=final_lr,
+                T_max=int(ipe_scale * num_epochs * iterations_per_epoch),
+        )
+
+
+        wd_scheduler = CosineWDSchedule(
+                optimizer,
+                ref_wd=weight_decay,
+                final_wd=final_weight_decay,
+                T_max=int(ipe_scale * num_epochs * iterations_per_epoch),
+            )
+
+        momentum_scheduler = (ema[0] + i*(ema[1]-ema[0])/(iterations_per_epoch*num_epochs*ipe_scale)
+                                for i in range(int(iterations_per_epoch*num_epochs*ipe_scale)+1))
+
+        scaler = torch.cuda.amp.GradScaler() if mixed_precision else None
+        load_file = os.listdir(checkpoint_folder)
+        sorted_filenames = sorted(load_file, key=lambda x: int(x.split('-')[0]))
+        load_path = os.path.join(checkpoint_folder, sorted_filenames[-1])
+        # load_path = '/sc/arion/projects/ECHO_ML/gulamf01/multimodal_LLM/src/1_pretrain-model/1.2.3_train-loop-gpu-huge/checkpoint/97-latest-pth.tar'
+        
+        print("Loading from file", load_path)
+        (
+            encoder,
+            predictor,
+            target_encoder,
+            optimizer,
+            scaler,
+            start_epoch,
+        ) = load_checkpoint( # Problem line!!
+            r_path=load_path,
+            encoder=encoder,
+            predictor=predictor,
+            target_encoder=target_encoder,
+            opt=optimizer,
+            scaler=scaler,
+            device=gpu_id)
+        print("Current on start epoch", start_epoch)
+        for _ in range(start_epoch * iterations_per_epoch):
+            scheduler.step()
+            wd_scheduler.step()
+            next(momentum_scheduler)
+            collator.step()
+        print(start_epoch)
+    
+    def _load_snapshot(snapshot_path):
+        snapshot = torch.load(snapshot_path)
+        encoder = (snapshot['ENCODER'])
+        predictor = (snapshot['PREDICTOR'])
+        target_encoder = (snapshot['TARGET_ENCODER'])
+        optimizer.load_state_dict(snapshot['OPTIMIZER'])
+        scaler.load_state_dict(snapshot['SCALER'])
+        start_epoch = snapshot['EPOCH']
+    
+    def _save_snapshot(rank, encoder, predictor, optimizer, scaler, target_encoder, epoch, batch_size, world_size, lr, path):
+        save_dict = {
+            'ENCODER': encoder.state_dict(),
+            'PREDICTOR': predictor.state_dict(),
+            'OPTIMIZER': optimizer.state_dict(),
+            'SCALER': None if scaler is None else scaler.state_dict(),
+            'TARGET_ENCODER': target_encoder.state_dict(),
+            'EPOCH': epoch,
+            #'loss': loss_meter.avg,
+            'BATCH_SIZE': batch_size,
+            'WORLD_SIZE': world_size,
+            'LEARNING_RATE': lr,
+            'RANK': rank
+        }
+        torch.save(save_dict, snapshot_path)
+    #if(os.path.exists(snapshot_path)):
+    #    _load_snapshot()
+    
+    # ddp_setup()
+    # print(torch.cuda.memory_summary())
+    
+
+    print("Rank", rank, "GPU-ID", gpu_id)
+    try:
+        encoder = encoder.to(gpu_id)
+        encoder = DDP(encoder, device_ids = [gpu_id],find_unused_parameters=True)
+
+        predictor = predictor.to(gpu_id)
+        predictor = DDP(predictor, device_ids = [gpu_id],find_unused_parameters=True)
+
+        target_encoder = target_encoder.to(gpu_id)
+        target_encoder = DDP(target_encoder, device_ids = [gpu_id],find_unused_parameters=True)
+        print("Succesfully loaded onto GPU", gpu_id)
+    except:
+        print("Failed to load on GPU", gpu_id, "Rank", rank)
+    #predictor.to(gpu_id)
+    #target_encoder.to(gpu_id)
+
+    print("Initializing optimizer.")
+    optimizer = pretrain_optimizer(
+        encoder=encoder, predictor=predictor, betas=betas, epsilon=epsilon, zero_init_bias_wd=zero_init_bias_wd
+    )
+    scaler = torch.cuda.amp.GradScaler() if mixed_precision else None
+
     print("Initializing scheduler.")
     dtype=torch.bfloat16
     scheduler = WarmupCosineSchedule(
@@ -242,7 +370,7 @@ def main(config_kwargs):
         ref_lr=ref_lr,
         final_lr=final_lr,
         T_max=int(ipe_scale * num_epochs * iterations_per_epoch),
-)
+    )
 
 
     wd_scheduler = CosineWDSchedule(
@@ -250,80 +378,11 @@ def main(config_kwargs):
             ref_wd=weight_decay,
             final_wd=final_weight_decay,
             T_max=int(ipe_scale * num_epochs * iterations_per_epoch),
-        )
+    )
 
     momentum_scheduler = (ema[0] + i*(ema[1]-ema[0])/(iterations_per_epoch*num_epochs*ipe_scale)
                             for i in range(int(iterations_per_epoch*num_epochs*ipe_scale)+1))
 
-    # Checkpointing
-    checkpoint_freq = config['checkpoint_frequency']
-    checkpoint_folder = config['checkpoint_folder']
-    load_model = config['load_model']
-    print("Loading from checkpoint.")
-    if load_model:
-        load_file = os.listdir(checkpoint_folder)
-        load_path = os.path.join(checkpoint_folder, load_file[-1])
-        (
-            encoder,
-            predictor,
-            target_encoder,
-            optimizer,
-            scaler,
-            start_epoch,
-        ) = load_checkpoint(
-            r_path=load_path,
-            encoder=encoder,
-            predictor=predictor,
-            target_encoder=target_encoder,
-            opt=optimizer,
-            scaler=scaler)
-        print("Current on start epoch", start_epoch)
-        for _ in range(start_epoch * iterations_per_epoch):
-            scheduler.step()
-            wd_scheduler.step()
-            next(momentum_scheduler)
-            collator.step()
-        print(start_epoch)
-    
-    def _load_snapshot(snapshot_path):
-        snapshot = torch.load(snapshot_path)
-        encoder.load_state_dict(snapshot['ENCODER'])
-        predictor.load_state_dict(snapshot['PREDICTOR'])
-        target_encoder.load_state_dict(snapshot['TARGET_ENCODER'])
-        optimizer.load_state_dict(snapshot['OPTIMIZER'])
-        scaler.load_state_dict(snapshot['SCALER'])
-        start_epoch = snapshot['EPOCH']
-    
-    def _save_snapshot(rank, encoder, predictor, optimizer, scaler, target_encoder, epoch, batch_size, world_size, lr, path):
-        save_dict = {
-            'ENCODER': encoder.module.state_dict(),
-            'PREDICTOR': predictor.module.state_dict(),
-            'OPTIMIZER': optimizer.state_dict(),
-            'SCALER': None if scaler is None else scaler.state_dict(),
-            'TARGET_ENCODER': target_encoder.module.state_dict(),
-            'EPOCH': epoch,
-            #'loss': loss_meter.avg,
-            'BATCH_SIZE': batch_size,
-            'WORLD_SIZE': world_size,
-            'LEARNING_RATE': lr,
-        }
-        torch.save(save_dict, snapshot_path)
-    if(os.path.exists(snapshot_path)):
-        _load_snapshot()
-    
-    ddp_setup(rank, world_size)
-
-    
-    encoder = encoder.to(rank)
-    encoder = DDP(encoder, device_ids = [rank])
-
-    predictor = predictor.to(rank)
-    predictor = DDP(predictor, device_ids = [rank])
-
-    target_encoder = target_encoder.to(rank)
-    target_encoder = DDP(target_encoder, device_ids = [rank])
-    #predictor.to(gpu_id)
-    #target_encoder.to(gpu_id)
 
     for epoch in range(start_epoch, num_epochs):
         unsupervised_sampler.set_epoch(epoch)
@@ -334,24 +393,33 @@ def main(config_kwargs):
         #itr = 1
         #ipe = 1
 
-
+        latent_list = []
+        images_list = []
         for itr in range(iterations_per_epoch):
             # print("Current iteration", current_itr)
             current_itr = int(itr + epoch*iterations_per_epoch)
+            '''
             udata, masks_enc, masks_pred = next(loader)
+            print("On iteration", itr)
+            '''
+            try:
+                udata, masks_enc, masks_pred = next(loader) # labels, clip_indices!
+            except StopIteration:
+                loader = iter(unsupervised_loader)
+                udata, masks_enc, masks_pred = next(loader)
 
             def load_clips():
                 # -- unsupervised video clips
                 # Put each clip on the GPU and concatenate along batch
                 # dimension
-                clips = torch.cat([u.to(device, non_blocking=True) for u in udata[0]], dim=0)
+                clips = torch.cat([u.to(gpu_id, non_blocking=True) for u in udata[0]], dim=0)
 
                 # Put each mask-enc/mask-pred pair on the GPU and reuse the
                 # same mask pair for each clip
                 _masks_enc, _masks_pred = [], []
                 for _me, _mp in zip(masks_enc, masks_pred):
-                    _me = _me.to(device, non_blocking=True)
-                    _mp = _mp.to(device, non_blocking=True)
+                    _me = _me.to(gpu_id, non_blocking=True)
+                    _mp = _mp.to(gpu_id, non_blocking=True)
                     _me = repeat_interleave_batch(_me, batch_size, repeat=num_clips)
                     _mp = repeat_interleave_batch(_mp, batch_size, repeat=num_clips)
                     _masks_enc.append(_me)
@@ -407,11 +475,11 @@ def main(config_kwargs):
                 pstd_z = reg_fn(z)  # predictor variance across patches
                 loss_reg += torch.mean(F.relu(1.-pstd_z))
                 loss = loss_jepa + reg_coeff * loss_reg
-
-                writer.add_scalar("Loss/JEPA", loss_jepa, current_itr)
-                # writer.add_scalar("Loss/PSTD", pstd_z, current_itr)
-                writer.add_scalar("Loss/Regularized", loss_reg, current_itr)
-                writer.add_scalar("Loss/total", loss, current_itr)
+                if(rank == 0):
+                    writer.add_scalar("Loss/JEPA", loss_jepa, current_itr)
+                    # writer.add_scalar("Loss/PSTD", pstd_z, current_itr)
+                    writer.add_scalar("Loss/Regularized", loss_reg, current_itr)
+                    writer.add_scalar("Loss/total", loss, current_itr)
 
                 # Step 2. Backward & step
                 _enc_norm, _pred_norm = 0., 0.
@@ -446,13 +514,28 @@ def main(config_kwargs):
             train_step()
 
 
-            if epoch % checkpoint_freq == 0 or epoch == (num_epochs - 1):
-                latest_file = f'{epoch}-latest-pth.tar'
-                latest_path = os.path.join(checkpoint_folder, latest_file)
-                
-                if(world_size == 1):
+        if epoch % checkpoint_freq == 0 or epoch == (num_epochs - 1):
+            latest_file = f'{epoch}-latest-pth.tar'
+            latest_path = os.path.join(checkpoint_folder, latest_file)
+            
+            if(world_size == 1):
+                save_checkpoint(
+                    rank=0, 
+                    encoder=encoder,
+                    predictor=predictor,
+                    optimizer=optimizer,
+                    scaler=scaler,
+                    target_encoder=target_encoder,
+                    epoch=epoch,
+                    batch_size=batch_size,
+                    world_size=world_size, 
+                    lr=start_lr,
+                    path= latest_path
+                )
+            else:
+                if(world_size != 1 and rank == 0):
                     save_checkpoint(
-                        rank=0, 
+                        rank=rank, 
                         encoder=encoder,
                         predictor=predictor,
                         optimizer=optimizer,
@@ -464,20 +547,19 @@ def main(config_kwargs):
                         lr=start_lr,
                         path= latest_path
                     )
-                else:
-                    if(world_size != 1 and rank == 0):
-                        _save_snapshot(
-                            encoder=encoder,
-                            predictor=predictor,
-                            optimizer=optimizer,
-                            scaler=scaler,
-                            target_encoder=target_encoder,
-                            epoch=epoch,
-                            batch_size=batch_size,
-                            world_size=world_size, 
-                            lr=start_lr,
-                            path= latest_path
-                        )
+                    _save_snapshot(
+                        rank=rank,
+                        encoder=encoder,
+                        predictor=predictor,
+                        optimizer=optimizer,
+                        scaler=scaler,
+                        target_encoder=target_encoder,
+                        epoch=epoch,
+                        batch_size=batch_size,
+                        world_size=world_size, 
+                        lr=start_lr,
+                        path= latest_path
+                    )
                         # save_checkpoint(
                         #     rank=0, 
                         #     encoder=encoder.module,
@@ -490,10 +572,6 @@ def main(config_kwargs):
                         #     world_size=world_size, 
                         #     lr=start_lr,
                         #     path= latest_path
-                # if save_every_freq > 0 and epoch % save_every_freq == 0:
-                #     save_every_file = f'{tag}-e{epoch}.pth.tar'
-                #     save_every_path = os.path.join(folder, save_every_file)
-                #     save_checkpoint(epoch + 1, save_every_path)
     destroy_process_group()
     return None
 
@@ -502,8 +580,14 @@ def ddp_setup():
     init_process_group(backend="nccl")
 
 
+def ddp_setup_original(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12346"
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Configuration arguments for convergence sample size estimtation.')
+    parser = argparse.ArgumentParser(description='Configuration arguments for pre-training.')
     
     parser.add_argument("--config_path", required=True, type=str,
                         help="Config file path.")
@@ -511,4 +595,8 @@ if __name__ == "__main__":
     config_kwargs = parser.parse_args()
 
     # Spawn the world size. 
-   main(config_kwargs)
+    world_size = 3
+    mp.spawn(main, args=(config_kwargs,), nprocs=world_size)
+
+    # With torchrun
+    # main(config_kwargs)
